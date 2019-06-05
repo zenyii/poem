3d5a38cef35540b0e9a1a54c30f1b663779cba5a
zenyi <1048677004@qq.com>
2019-06-05 17:57:54 +0800

    兜底上传

--- poem/miniprogram/pages/sort/sort.js
+++ poem/miniprogram/pages/sort/sort.js
@@ -9,8 +9,8 @@ Page({
     kind: [
       { class: "朝代", url: "../../images/sort1.png" },
       { class: "作者", url: "../../images/sort2.png" }, 
-      { class: "诗集", url: "../../images/sort3.png" },
-      { class: "季节", url: "../../images/sort4.png"}]
+      { class: "类型", url: "../../images/sort3.png" },
+      { class: "诗集", url: "../../images/sort4.png"}]
   },
 
   /**
